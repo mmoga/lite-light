e0d5d0aab05da489117543871057f90f4ce6f751
Matthew Mogavero <matt.moga@gmail.com>
2018-02-01 18:53:41 -0500

    created the size selection form as well as wired up a submit listener on the form

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 //variables
 const resetBtn = document.querySelector('#reset'); // querySelector means just one
 const boxes = document.querySelectorAll('.box');   // vs querySelectorAll
+const gridSizeForm = document.querySelector('#grid-size');
 
 //eventListeners
 boxes.forEach(function(box, index){ //note the use of an inline function
     box.addEventListener('click', toggleRed);
-})
-resetBtn.addEventListener('click', clearGrid)
+});
+resetBtn.addEventListener('click', clearGrid);
+gridSizeForm.addEventListener('submit', changeGrid);
 
 //functions
 function toggleRed(e) {
@@ -18,4 +20,10 @@ function clearGrid(e) {
     boxes.forEach((box, index) => { //note the use of a 'fat arrow' function
         box.classList.remove('red');
     });
+}
+
+function changeGrid(e) {
+    e.preventDefault();
+    const gridSizeValue = +gridSizeForm.querySelector('input:checked').value;
+    console.log(e);
 }
